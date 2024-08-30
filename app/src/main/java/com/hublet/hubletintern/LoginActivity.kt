f1303f9e9bcccf572a03f97fc95cc79d3dabc9d2
Marzia Rashid <marziarashid@Marzias-Air.lan>
2024-08-30 12:05:10 +0300

    Activity and layout design for login and signup

--- app/src/main/java/com/hublet/hubletintern/LoginActivity.kt
+++ app/src/main/java/com/hublet/hubletintern/LoginActivity.kt
@@ -3,35 +3,34 @@ package com.hublet.hubletintern
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+class LoginActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity() {
-    private var btnSignIn: Button? = null
     private var txtCreateAcc: TextView? = null
+    private var btnLogin: TextView? = null
 
-    @SuppressLint("CutPasteId", "MissingInflatedId")
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        btnSignIn = findViewById(R.id.btn_sign_in)
+        setContentView(R.layout.activity_login)
         txtCreateAcc = findViewById(R.id.txt_sign_in)
+        btnLogin = findViewById(R.id.btn_login)
 
-        btnSignIn?.setOnClickListener {
-            Toast.makeText(this, "Sign in", Toast.LENGTH_LONG).show()
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
 
         txtCreateAcc?.setOnClickListener {
             Toast.makeText(this, "Create Account", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this@MainActivity, SignInActivity::class.java)
+            val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
+        btnLogin?.setOnClickListener {
+            Toast.makeText(this, "User Profile", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, UserProfileActivity::class.java)
+            startActivity(intent)
+        }
+
 
     }
 }
